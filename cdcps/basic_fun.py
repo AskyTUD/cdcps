7e9ed332b4f51cf7e5c6da9ee5cb54a24bb7ab1d
Andreas Himmel <andreas.himmel@iat.tu-darmstadt.de>
2022-12-13 18:26:02 +0100

    added color options

--- cdcps/basic_fun.py
+++ cdcps/basic_fun.py
@@ -21,10 +21,23 @@
 #   along with cdcps. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import random
 import numpy as np
 
-def vercat(ma_list):
-    return np.array([[ii_number] for i_element in ma_list for ii_number in i_element])
+def vercat(*args):
+    return np.array([[ii_number] for i_element in args for ii_number in i_element])
 
+def colors_generator(firstval=0, step=1):
+    # colors = itertools.cycle(inferno(self.synchronization["graphs"][0].graph.number_of_nodes()))# create a color iterator
+    colorsA = ["black", "darkslategray", "slategray", "lightslategray", "dimgray", "darkgray"]
+    colorsB = ["midnightblue", "navy", "steelblue", "lightslategray", "cornflowerblue", "skyblue"]
+    colorsC = ["darkred", "firebrick", "indianred"]
+    colorsD = ["purple", "indigo", "darkmagenta", "darkorange"]
+    colorsE = ["darkgreen", "seagreen", "mediumseagreen", "darkseagreen", "darkolivegreen", "olivedrab", "teal"]
+    colorstot = colorsA + colorsB + colorsC + colorsD + colorsE
+    xx = firstval
+    yield random.sample(colorstot, colorstot.__len__())[xx]
+    xx += step
 
-
+def background_color():
+    return 'darkgray' #'slategray' #'darkslategray'
